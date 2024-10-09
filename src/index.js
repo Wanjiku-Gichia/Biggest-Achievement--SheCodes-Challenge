397e45c977600c2a7b1c0c7084de753b2a48f6b2
Wanjiku-Gichia <77983272+Wanjiku-Gichia@users.noreply.github.com>
2024-10-09 14:29:58 +0300

    Added a form to collect feedback

--- src/index.js
+++ src/index.js
@@ -5,3 +5,17 @@ function changeTheme(params) {
 
 let darkTheme = document.querySelector(".darkTheme");
 darkTheme.addEventListener("click", changeTheme);
+
+function userFeedback(event) {
+  event.preventDefault();
+  let feedbackInput = document.querySelector("#userFeedback");
+  let feedback = feedbackInput.value;
+  if (feedback.length > 0) {
+    alert(`Thank you! Your feedback ("${feedback}") is very much appreciated.`);
+    feedbackInput.value = "";
+  } else {
+    alert("please 🙏 give me some feedbak to help improve my project.");
+  }
+}
+let feedback = document.querySelector("#submitFeedback");
+feedback.addEventListener("click", userFeedback);
